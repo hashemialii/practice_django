92be273bbb0b9b8a2f4100fda56ab4dfc269e17b
ali <alihashemi9675@gmail.com>
2024-09-22 19:58:45 +0330

    Practice

--- posts/admin.py
+++ posts/admin.py
@@ -5,17 +5,19 @@ from .models import Post, Comment
 class CommentAdminInline(admin.TabularInline):
     model = Comment
     fields = ['text', ]
+    extra = 0
 
 
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'is_enable', 'publish_date', 'created_time']
     inlines = [CommentAdminInline, ]
 
 
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ['post', 'text', 'created_time']
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ['post', 'text', 'created_time']
 
 
-admin.site.register(Post, PostAdmin)
+# admin.site.register(Post, PostAdmin)
 # admin.site.register(Comment, CommentAdmin)
 
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Comment
 
 
 def index(request):
@@ -11,3 +12,16 @@ def home(request):
 
     return HttpResponse('<h3> Welcome to my site</h3>')
 
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/post_list.html', context=context)
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    # comments = Comment.objects.filter(post=post_id)
+    comments = Comment.objects.filter(post=post)
+    context = {'post': post, 'comments': comments}
+    return render(request, 'posts/post_detail.html', context=context)
