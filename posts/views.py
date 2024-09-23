990cfd3462652fb1a8349c0ec20a67ca022aa1e3
ali <alihashemi9675@gmail.com>
2024-09-23 18:36:49 +0330

    Practice

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from .models import Post, Comment
+from .forms import PostForm
 
 
 def index(request):
@@ -20,8 +21,28 @@ def post_list(request):
 
 
 def post_detail(request, post_id):
-    post = Post.objects.get(pk=post_id)
+    # try:
+    #     post = Post.objects.get(pk=post_id)
+    # except Post.DoesNotExist:
+    #     return HttpResponseNotFound('Post in not exist!')
+    post = get_object_or_404(Post, pk=post_id)
+
     # comments = Comment.objects.filter(post=post_id)
     comments = Comment.objects.filter(post=post)
     context = {'post': post, 'comments': comments}
     return render(request, 'posts/post_detail.html', context=context)
+
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            print(type(form.cleaned_data))
+            print(form.cleaned_data)
+            Post.objects.create(**form.cleaned_data)
+            return HttpResponseRedirect('/posts/')
+    else:
+        form = PostForm()
+
+    return render(request, 'posts/post_create.html', {'form': form})
+
--- weblog/urls.py
+++ weblog/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from posts.views import index, home, post_list, post_detail
+from posts.views import index, home, post_list, post_detail, post_create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index),
     path('home/', home),
     path('posts/', post_list, name='post-list'),
+    path('posts/create/', post_create),
     path('posts/<int:post_id>/', post_detail, name='post-detail'),
 ]
